2234383275a0d23cce5aea12e09994a8d78c4b85
Max Holland <mholland@brightcove.com>
2020-06-01 15:44:30 +0100

    vjs 5 code

--- custom.js
+++ custom.js
@@ -1,37 +1,25 @@
-console.log('hello')    
+console.log('hello')
 
-videojs.newButton = videojs.Button.extend({
-       init: function(player, options){
-        videojs.Button.call(this, player, options);
-        this.on('click', this.onClick);
-       }
-    });
-
-    videojs.newButton.prototype.onClick = function() {
+var vjsButtonComponent = videojs.getComponent('Button');
+videojs.registerComponent('SkipButton', videojs.extend(vjsButtonComponent, {
+    constructor: function () {
+        vjsButtonComponent.apply(this, arguments);
+    },
+    handleClick: function () {
         videojs.getPlayer('vjs_video_3').currentTime(18)
-    };
+    },
+    buildCSSClass: function () {
+        return 'vjs-control vjs-download-button';
+    },
+    createControlTextEl: function (button) {
+        return $(button).html($('<span class="glyphicon glyphicon-download-alt"></span>').attr('title', 'Skip'));
+    }
+}));
 
-     //Creating New Button
-    var createNewButton = function() {
-        var props = {
-            className: 'vjs-new-button vjs-control',
-            innerHTML: '<div class="vjs-control-content">' + ('New') + '</div>',
-            role: 'button',
-            'aria-live': 'polite', 
-            tabIndex: 0
-            };
-        return videojs.Component.prototype.createEl(null, props);
-    };
-
-    //Adding the newly created button to Control Bar
-
-    videojs.plugin('newButton', function() {
-        var options = { 'el' : createNewButton() };
-        newButton = new videojs.newButton(this, options);
-        this.controlBar.el().appendChild(newButton.el());
-    });
-
-videojs("vjs_video_3", {
-        plugins : { newButton : {} }
-    });
-
+videojs(
+    'player-id',
+    {fluid: true},
+    function () {
+        this.getChild('controlBar').addChild('SkipButton', {});
+    }
+);
