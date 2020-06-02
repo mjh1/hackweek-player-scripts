7ef4218362bd757a181f2a9592a8a1db2ebb206d
Max Holland <mholland@brightcove.com>
2020-06-02 17:36:21 +0100

    get skip time from custom fields

--- custom.js
+++ custom.js
@@ -1,12 +1,11 @@
 
-videojs("vjs_video_3").ready(function(){
 var vjsButtonComponent = videojs.getComponent('Button');
 videojs.registerComponent('SkipButton', videojs.extend(vjsButtonComponent, {
     constructor: function () {
         vjsButtonComponent.apply(this, arguments);
     },
     handleClick: function () {
-        videojs.getPlayer('vjs_video_3').currentTime(25)
+        videojs.getPlayer('vjs_video_3').currentTime(videojs.getPlayer('vjs_video_3').mediainfo.custom_fields.skip_marker)
     },
     buildCSSClass: function () {
         return 'vjs-control vjs-download-button';
@@ -17,4 +16,3 @@ videojs.registerComponent('SkipButton', videojs.extend(vjsButtonComponent, {
 }));
 
 videojs.getPlayer('vjs_video_3').getChild('controlBar').addChild('SkipButton', {})
-})
