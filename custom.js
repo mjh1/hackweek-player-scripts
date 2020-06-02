e129708f2e5d35c6487e1b7f831265f2cf3fd266
Max Holland <mholland@brightcove.com>
2020-06-02 08:59:07 +0100

    move script into separate file

--- custom.js
+++ custom.js
@@ -1,27 +1,17 @@
-console.log('hello')
-
-//videojs('vjs_video_3_html5_api')
-
-/*var vjsButtonComponent = videojs.getComponent('Button');
+var vjsButtonComponent = videojs.getComponent('Button');
 videojs.registerComponent('SkipButton', videojs.extend(vjsButtonComponent, {
     constructor: function () {
         vjsButtonComponent.apply(this, arguments);
     },
     handleClick: function () {
-        videojs.getPlayer('vjs_video_3').currentTime(18)
+        videojs.getPlayer('vjs_video_3').currentTime(25)
     },
     buildCSSClass: function () {
         return 'vjs-control vjs-download-button';
     },
     createControlTextEl: function (button) {
-        return $(button).html($('<span class="glyphicon glyphicon-download-alt"></span>').attr('title', 'Skip'));
+        return $(button).html($('<span class="glyphicon glyphicon-download-alt">Skip Intro</span>').attr('title', 'Skip'));
     }
 }));
 
-videojs(
-    'vjs_video_3',
-    {fluid: true},
-    function () {
-        this.getChild('controlBar').addChild('SkipButton', {});
-    }
-);*/
+videojs.getPlayer('vjs_video_3').getChild('controlBar').addChild('SkipButton', {})
