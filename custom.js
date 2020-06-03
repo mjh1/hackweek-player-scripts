03471b37fbf70a921ab4a9c8f1ca11524d6516ae
Max Holland <mholland@brightcove.com>
2020-06-03 16:11:49 +0100

    improve look and feel

--- custom.js
+++ custom.js
@@ -1,18 +1,25 @@
+var player
+videojs.getPlayer('vjs_video_3').ready(function() {
+  'use strict';
+  player = this
 
-var vjsButtonComponent = videojs.getComponent('Button');
-videojs.registerComponent('SkipButton', videojs.extend(vjsButtonComponent, {
-    constructor: function () {
-        vjsButtonComponent.apply(this, arguments);
-    },
-    handleClick: function () {
-        videojs.getPlayer('vjs_video_3').currentTime(videojs.getPlayer('vjs_video_3').mediainfo.custom_fields.skip_marker)
-    },
-    buildCSSClass: function () {
-        return 'vjs-control vjs-download-button';
-    },
-    createControlTextEl: function (button) {
-        return $(button).html($('<span class="glyphicon glyphicon-download-alt">Skip Intro</span>').attr('title', 'Skip'));
-    }
-}));
+  player.on("play", function(){
+    // var skipButton = $('<span onclick class="overlay-button-mjh">Skip Intro</span>')
 
-videojs.getPlayer('vjs_video_3').getChild('controlBar').addChild('SkipButton', {})
+    player.overlay({
+      overlays: [{
+        align: 'bottom-left',
+        showBackground: false,
+        attachToControlBar: true,
+        content: '<span id="skipButton" onclick class="overlay-button-mjh">Skip Intro</span>',
+        start: 1, // TODO add field for start of intro
+        end: parseInt(player.mediainfo.custom_fields.skip_marker)
+      }]
+    });
+
+    $('#skipButton').click(function() {
+      player.currentTime(player.mediainfo.custom_fields.skip_marker)
+    });
+  })
+
+})
